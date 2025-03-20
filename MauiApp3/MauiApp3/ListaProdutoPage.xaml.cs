c914de4c32e2d9dabc55ce21464f9bb274c6f0db
Prof. Alex <prof.alex.etec.2021@gmail.com>
2025-03-20 16:12:19 -0300

    Lista com click

--- MauiApp3/MauiApp3/ListaProdutoPage.xaml.cs
+++ MauiApp3/MauiApp3/ListaProdutoPage.xaml.cs
@@ -4,6 +4,16 @@ public partial class ListaProdutoPage : ContentPage
 {
 	public ListaProdutoPage()
 	{
+		List<Produto> lista = Produto.Lista;
+
 		InitializeComponent();
-	}
+
+        lstProduto.ItemsSource = lista;
+    }
+
+    private void ViewCell_Tapped(object sender, EventArgs e)
+    {
+
+        Navigation.PushAsync(new ProdutoPage() { BindingContext = ((ViewCell)sender).BindingContext });
+    }
 }
--- MauiApp3/MauiApp3/ProdutoPage.xaml.cs
+++ MauiApp3/MauiApp3/ProdutoPage.xaml.cs
@@ -3,7 +3,7 @@ namespace MauiApp3;
 public partial class ProdutoPage : ContentPage
 {
 	public ProdutoPage()
-	{
+	{	
 		InitializeComponent();
 	}
 }
